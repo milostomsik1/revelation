545ffbd71eec14397fa7874bd8c608613fa71bb9
Milos Tomsik <milostomsik1@gmail.com>
2017-05-06 11:36:23 +0200

    hero img locally, main.js optimization

--- code/js/main.js
+++ code/js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 	$('.match-footer-box-height').matchHeight();
-});
 
-$(document).ready(function() {
 	$('a#mobile-nav').on("click", function() {
 		$("ul#mobile-menu").toggleClass("expand");
 	});
--- code_final/js/main.js
+++ code_final/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".match-footer-box-height").matchHeight()}),$(document).ready(function(){$("a#mobile-nav").on("click",function(){$("ul#mobile-menu").toggleClass("expand")})});
+$(document).ready(function(){$(".match-footer-box-height").matchHeight(),$("a#mobile-nav").on("click",function(){$("ul#mobile-menu").toggleClass("expand")})});
